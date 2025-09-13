99c8e7963b7dd24406fb087b7396ee1db3dba7ed
richellestoweres-maker <richellestowe.res@gmail.com>
2025-09-13 16:59:14 -0500

    Update ft-firebase.js

--- ft-firebase.js
+++ ft-firebase.js
@@ -1,20 +1,15 @@
-// /js/ft-firebase.js
+// /ft-firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-storage.js";
+// (optional) Storage later:
+// import { getStorage } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCfMOyKa2uT6BuMiST95Ry96Z7FUH_vYgQ",
-  authDomain: "the-finishing-touch-8ac71.firebaseapp.com",
-  projectId: "the-finishing-touch-8ac71",
-  storageBucket: "the-finishing-touch-8ac71.firebasestorage.app",
-  messagingSenderId: "591177706994",
-  appId: "1:591177706994:web:254424c220c5d15fa24f6b",
-  measurementId: "G-2SR4MB8JHW"
+  // your keys here (the ones you pasted earlier)
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+export const db   = getFirestore(app);
+// export const storage = getStorage(app); // later when you need photos
